006914c755a340ec65945278483e942b49a7895f
Aizad Ridzo <aizad@deriv.com>
2022-11-07 11:01:33 +0800

    hotfix: final changes before final push

--- src/javascript/app/pages/user/account/settings/limits/limits.ui.js
+++ src/javascript/app/pages/user/account/settings/limits/limits.ui.js
@@ -55,7 +55,7 @@ const LimitsUI = (() => {
                     appendRowTable(markets[market].name, '', 'auto', 'bold');
                     limits.market_specific[market].forEach((submarket) => {
                         // submarket name could be (Commodities|Minor Pairs|Major Pairs|Smart FX|Stock Indices|Synthetic Indices)
-                        if (submarket.name !== 'Forex' && submarket.level !== 'market') {
+                        if (!(submarket.name === 'Forex' && submarket.level === 'market')) {
                             appendRowTable(submarket.name, submarket.turnover_limit !== 'null' ? Currency.formatMoney(currency, submarket.turnover_limit, 1) : 0, '25px', 'normal');
                         }
                     });
@@ -64,7 +64,7 @@ const LimitsUI = (() => {
             if (limits.market_specific.synthetic_index) {
                 appendRowTable(localize('Derived'), '', 'auto', 'bold');
                 limits.market_specific.synthetic_index.forEach((submarket) => {
-                    if (submarket.level === 'submarket') {
+                    if (submarket.level !== 'submarket') {
                         appendRowTable(submarket.name, submarket.turnover_limit !== 'null' ? Currency.formatMoney(currency, submarket.turnover_limit, 1) : 0, '25px', 'normal');
                     }
                 });
